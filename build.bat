0a266916da765f66eefd65aa9dbd42cf7a316bd8
aldous <mail@aldous.ch>
2024-12-10 13:41:03 +0100

    feat: windows support

--- build.bat
+++ build.bat
@@ -1,6 +1,12 @@
 @echo off
 
-rem https://github.com/floooh/sokol-tools/blob/master/docs/sokol-shdc.md
-sokol-shdc -i source/shader.glsl -o source/shader.odin -l hlsl5:wgsl -f sokol_odin
+if not exist "build" mkdir build
 
-odin build source -debug
+rem https://github.com/floooh/sokol-tools/blob/master/docs/sokol-shdc.md
+sokol-shdc -i src/shader.glsl -o src/shader.odin -l hlsl5:wgsl -f sokol_odin
+
+rem build the shit
+odin build src -debug -out:build/game.exe
+
+rem run the shit
+build\game.exe
